3ae0e22d17be6ba8d7a4027cffcbb7c4cc428b56
Franco Veron Peralta <francoveronperalta@gmail.com>
2024-06-13 02:32:47 -0300

    2da entrega

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const sections = [
 	{ title: 'Experiencia Profesional', id: 'experience' },
 ]
 
+const aboutDescription = [
+	'Soy un apasionado Desarrollador de Software con una sólida experiencia en la creación de soluciones web innovadoras.',
+	'Me especializo en el desarrollo fullstack con un enfoque centrado en el usuario, creando experiencias digitales excepcionales.',
+	'Me destaco por combinar habilidades técnicas y creativas para ofrecer soluciones eficientes y escalables.',
+]
+
 const skills = [
 	'HTML',
 	'CSS | SASS | Tailwind',
@@ -49,7 +55,7 @@ const profesionalExperience = {
 			items: [
 				'Contribuir a diseñar la nueva estructura para proporcionar información centralizada a 37 sucursales en todo el país.',
 				'Desarrollar una API REST utilizando ASP.NET MVC.',
-				'Detectar y resolví errores en los métodos utilizados para recuperar información de la base de datos',
+				'Detectar y resolver errores en los métodos utilizados para recuperar información de la base de datos',
 			],
 		},
 	],
@@ -68,16 +74,41 @@ const subDivideArray = (arr, divider) => {
 }
 
 const navbar = document.querySelector('header nav')
-const ulSections = document.createElement('ul')
+const aboutContainer = document.querySelector('#about > div:nth-of-type(2)')
 const skillsContainer = document.getElementById('skills_container')
 const otherSkillsContainer = document.getElementById('other_skills_container')
+const job = document.getElementsByClassName('job')[0]
+const buttonsToggles = document.querySelectorAll('.fa-arrow-down')
+
+buttonsToggles.forEach(e => {
+  const parentElement = e.parentElement
+  const dataElement = parentElement.nextElementSibling
+
+  console.log(dataElement)
+
+  e.addEventListener('click', () => {
+    dataElement.style.display = dataElement.style.display === 'none' ? 'block' : 'none'
+  })
+}
+)
+
 
 const insertDataOnNavbar = () => {
+	const ulSections = document.createElement('ul')
 	navbar.appendChild(ulSections)
 	sections.forEach(e => {
 		const li = document.createElement('li')
 		li.innerHTML = `<a href="#${e.id}" class="nav_item">${e.title}</a>`
 		ulSections.appendChild(li)
+	})
+}
+
+const insertDataOnAboutContainer = () => {
+  aboutDescription.forEach(e => {
+		const p = document.createElement('p')
+		p.innerText = e
+
+		aboutContainer.appendChild(p)
 	})
 }
 
@@ -100,25 +131,73 @@ const insertDataOnSkillsContainer = () => {
 }
 
 const insertDataOnOtherSkillsContainer = () => {
-  const otherSkillsColumArr = subDivideArray(otherSkills, 2)
+	const otherSkillsColumArr = subDivideArray(otherSkills, 2)
 
-  otherSkillsColumArr.forEach(e => {
-    const ul = document.createElement('ul')
-    ul.className = 'other_skills_column'
+	otherSkillsColumArr.forEach(e => {
+		const ul = document.createElement('ul')
+		ul.className = 'other_skills_column'
 
-    e.forEach(element => {
-      const li = document.createElement('li')
-      li.className = 'other_skill_item'
-      li.innerText = element
-      ul.appendChild(li)
-    })
+		e.forEach(element => {
+			const li = document.createElement('li')
+			li.className = 'other_skill_item'
+			li.innerText = element
+			ul.appendChild(li)
+		})
 
-    otherSkillsContainer.appendChild(ul)
-  })
+		otherSkillsContainer.appendChild(ul)
+	})
+}
+
+const insertDataOnJob = () => {
+	const jobHeader = document.createElement('div')
+	jobHeader.className = 'job_header'
+
+	const jobTitle = document.createElement('h5')
+	jobTitle.className = 'job_title'
+	jobTitle.innerHTML = `<strong>${profesionalExperience.title}</strong> | ${profesionalExperience.position}`
+
+	const jobData = document.createElement('span')
+	jobData.className = 'job_data'
+	jobData.innerHTML = `${profesionalExperience.location} | <strong>${profesionalExperience.date}</strong>`
+
+	jobHeader.appendChild(jobTitle)
+	jobHeader.appendChild(jobData)
+
+	job.appendChild(jobHeader)
+
+	profesionalExperience.projects.forEach(e => {
+		const jobDescription = document.createElement('div')
+		jobDescription.className = 'job_description'
+
+		const jobDescriptionP = document.createElement('p')
+		jobDescriptionP.innerHTML = `<span>${e.projectTitle}:</span> ${e.description}`
+		jobDescription.appendChild(jobDescriptionP)
+
+		const jobDescriptionUl = document.createElement('ul')
+		jobDescription.appendChild(jobDescriptionUl)
+
+		e.items.forEach(element => {
+			const li = document.createElement('li')
+			li.innerText = element
+			jobDescriptionUl.appendChild(li)
+		})
+
+		job.appendChild(jobDescription)
+	})
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+	    const toggleButton = document.getElementById('toggle-theme')
+		const body = document.body
+
+		toggleButton.addEventListener('click', () => {
+			body.classList.toggle('light-mode')
+		})
+
 	insertDataOnNavbar()
-  insertDataOnSkillsContainer()
-  insertDataOnOtherSkillsContainer()
+	insertDataOnAboutContainer()
+	insertDataOnSkillsContainer()
+	insertDataOnOtherSkillsContainer()
+  insertDataOnJob()
+  
 })
